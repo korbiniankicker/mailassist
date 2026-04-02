f156eb565454870bacf7922b1486553753785b31
Korbinian Kicker <korbinian@kicker.cc>
2026-04-02 22:12:35 +0200

    fix(email-embedder): fixed critical chunking issue that made chunks too large

--- backend/nestjs-rag/src/email-embedder/email-embedder.service.ts
+++ backend/nestjs-rag/src/email-embedder/email-embedder.service.ts
@@ -19,25 +19,9 @@ export class EmailEmbedderService {
     progress: number;
   }> {
     for await (let email of this.emailFetcherService.getMessages('INBOX')) {
-      if (
-        !email.message.sender ||
-        !email.message.content ||
-        !email.message.date ||
-        !email.message.subject
-      ) {
-        console.log(
-          'unable to fully fetch email: (DATE)' +
-            email.message.date +
-            ': (SUBJECT)' +
-            email.message.subject +
-            ': (SENDER)' +
-            email.message.sender +
-            ': (CONTENT)' +
-            email.message.content,
-        );
-        continue;
-      }
-      email.message.content = this.stripContent(email.message.content);
+      email.message.content = this.stripContentWhitespaces(
+        email.message.content,
+      );
       yield {
         emailDto: email.message,
         progress: email.progress,
@@ -45,7 +29,7 @@ export class EmailEmbedderService {
     }
   }
 
-  private stripContent(content: string): string {
+  private stripContentWhitespaces(content: string): string {
     const pattern = /^\s*[\r\n]/gm;
     return content.replace(pattern, '');
   }
@@ -53,34 +37,38 @@ export class EmailEmbedderService {
   private async *chunkEmails(): AsyncGenerator<{
     obj: {
       emailDto: EmailDto;
+      chunkedText: string;
       embedding: number[];
     };
     progress: number;
   }> {
     for await (const mail of this.filterEmails()) {
-      let buffer = `From: ${mail.emailDto.sender}\n
-                    Date: ${mail.emailDto.date}\n
-                    Subject: ${mail.emailDto.subject}\n
-                    Content: ${mail.emailDto.content}`;
-      while (buffer.length > CHUNK_SIZE + OVERLAP_SIZE * 2) {
-        const chunk = buffer.slice(0, CHUNK_SIZE + OVERLAP_SIZE * 2);
-        buffer = buffer.slice(CHUNK_SIZE + OVERLAP_SIZE);
+      let buffer = mail.emailDto.content;
+      let metadata = `From: ${mail.emailDto.sender} | Date: ${mail.emailDto.date} | Subject: ${mail.emailDto.subject}\n`;
+      while (buffer.length > CHUNK_SIZE) {
+        const chunk = metadata + `\n${buffer.slice(0, CHUNK_SIZE)}\n`;
+        buffer = buffer.slice(CHUNK_SIZE - OVERLAP_SIZE);
         const embedding = await this.ollamaService.getEmbedding(
-          'search_document :' + chunk,
+          'search_document: ' + chunk,
         );
         yield {
           obj: {
             emailDto: mail.emailDto,
+            chunkedText: chunk,
             embedding: embedding,
           },
           progress: mail.progress,
         };
       }
       if (buffer.length > 0) {
-        const embedding = await this.ollamaService.getEmbedding(buffer);
+        buffer = metadata + `${buffer}\n`;
+        const embedding = await this.ollamaService.getEmbedding(
+          'search_document: ' + buffer,
+        );
         yield {
           obj: {
             emailDto: mail.emailDto,
+            chunkedText: buffer,
             embedding: embedding,
           },
           progress: mail.progress,
@@ -91,13 +79,14 @@ export class EmailEmbedderService {
   private async *storeEmailEmbeddings(): AsyncGenerator<number> {
     for await (let chunk of this.chunkEmails()) {
       let emailChunk: EmailChunk = {
-        date: chunk.obj.emailDto.date ?? new Date(),
+        date: chunk.obj.emailDto.date,
         embedding: chunk.obj.embedding,
-        embeddedText: chunk.obj.emailDto.content ?? ' ',
-        sender: chunk.obj.emailDto.sender ?? ' ',
-        subject: chunk.obj.emailDto.subject ?? ' ',
+        embedded_text: chunk.obj.chunkedText,
+        sender: chunk.obj.emailDto.sender,
+        subject: chunk.obj.emailDto.subject,
+        message_id: chunk.obj.emailDto.messageId,
       } as EmailChunk;
-      this.emailStoreService.storeChunk(emailChunk);
+      await this.emailStoreService.storeChunk(emailChunk);
       yield chunk.progress;
     }
   }
