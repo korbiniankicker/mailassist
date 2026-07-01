201d8c9229467cfd7995a4ac8e9f56e0e1251757
Korbinian Kicker <korbinian@kicker.cc>
2026-07-01 23:48:33 +0200

    feat: added auth guards

--- backend/rag-microservice/src/chat/chat.gateway.ts
+++ backend/rag-microservice/src/chat/chat.gateway.ts
@@ -6,20 +6,27 @@ import {
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
-import { Logger } from '@nestjs/common';
+import { Logger, UseGuards } from '@nestjs/common';
+import { WsAuthGuard } from '../auth/wsauth.guard';
+import { QueryDto } from '../common/dto/query.dto';
 
 @WebSocketGateway({ cors: true, namespace: '/api' })
 export class ChatGateway {
   private readonly logger = new Logger(ChatGateway.name);
   constructor(private readonly chatService: ChatService) {}
 
+  @UseGuards(WsAuthGuard)
   @SubscribeMessage('query')
   async handleMessage(
-    @MessageBody() data: { prompt: string },
+    @MessageBody() data: QueryDto,
     @ConnectedSocket() client: Socket,
   ) {
     let response: string = '';
-    for await (let chunk of this.chatService.generateResponse(data.prompt)) {
+    for await (let chunk of this.chatService.generateResponse(
+      data.prompt,
+      client.data.user.id,
+      data.conversation_id,
+    )) {
       client.emit('response', chunk);
       response += chunk;
     }
--- backend/rag-microservice/src/email-embedder/email-embedder.gateway.ts
+++ backend/rag-microservice/src/email-embedder/email-embedder.gateway.ts
@@ -5,13 +5,18 @@ import {
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { EmailEmbedderService } from './email-embedder.service';
+import { UseGuards } from '@nestjs/common';
+import { WsAuthGuard } from '../auth/wsauth.guard';
 
+@UseGuards(WsAuthGuard)
 @WebSocketGateway({ cors: true, namespace: '/api' })
 export class EmailEmbedderGateway {
   constructor(private readonly emailEmbedderService: EmailEmbedderService) {}
   @SubscribeMessage('ingest')
   async handleMessage(@ConnectedSocket() client: Socket) {
-    for await (let progress of this.emailEmbedderService.embedEmails()) {
+    for await (let progress of this.emailEmbedderService.embedEmails(
+      client.data.user.id,
+    )) {
       client.emit('progress', progress);
     }
   }
